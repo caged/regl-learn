e0f6c85867e3c449c6f8756fb6f5248500137327
Caged <justin@labratrevenge.com>
2018-05-06 09:11:49 -0700

    move into single file for easy editing.  chagne color in loop

--- regl-hello-world/index.js
+++ regl-hello-world/index.js
@@ -1,20 +1,39 @@
 const regl = require("regl")(document.body);
-const shader = require("./index.shader");
 
-shader.on("change", () => {
-  console.log("Shader updated");
-});
+const drawTriangle = regl({
+  frag: `
+    precision mediump float;
+    uniform vec4 color;
 
-regl({
-  frag: shader.fragment,
-  vert: shader.vertex,
+    void main() {
+      gl_FragColor = color;
+    }
+  `,
+  vert: `
+    precision mediump float;
+    attribute vec2 position;
+
+    void main() {
+      gl_Position = vec4(position, 0, 1);
+    }
+  `,
   attributes: {
-    position: [[-1, 1], [1, 1], [1, -1], [-1, -1]]
+    position: [[-1, 1], [1, 1], [1, -1]]
   },
 
   uniforms: {
-    color: [1, 0, 0, 1]
+    color: regl.prop("color")
   },
 
-  count: 4
-})();
+  count: 3
+});
+
+const loop = regl.frame(({ time }) => {
+  const color = [
+    Math.cos(time * 0.1),
+    Math.sin(time * 0.8),
+    Math.cos(time * 0.003),
+    1
+  ];
+  drawTriangle({ color });
+});
