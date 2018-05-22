706e4fb4d3cb3662a4c7dbf0cc8680497b5dce23
Caged <justin@labratrevenge.com>
2018-05-22 08:13:31 -0700

    fix hello-world to work with budo

--- 00-hello-world/index.js
+++ 00-hello-world/index.js
@@ -1,7 +1,24 @@
-const c = document.getElementById('c')
-const gl = c.getContext('webgl')
+// Vertex shaders are responsible for setting the position of pixels on the
+// screen by setting `gl_Position`.
+const vertex = `
+  attribute vec4 a_position;
 
-function createShader(gl, type, source) {
+  void main() {
+    gl_Position = a_position;
+  }
+`
+
+// Fragment shaders are responsible for setting the color of pixels on screen by
+// setting `gl_FragColor`
+const fragment = `
+  precision mediump float;
+
+  void main() {
+    gl_FragColor = vec4(1, 0, 0.5, 1);
+  }
+`
+
+const createShader = (gl, type, source) => {
   const shader = gl.createShader(type)
   gl.shaderSource(shader, source)
   gl.compileShader(shader)
@@ -13,8 +30,12 @@ function createShader(gl, type, source) {
   gl.deleteShader(shader)
 }
 
-const vsource = document.getElementById('vertex').text
-const fsource = document.getElementById('frag').text
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.createElement('canvas')
+  const gl = canvas.getContext('webgl')
+  const vshader = createShader(gl, gl.VERTEX_SHADER, vertex)
+  const fshader = createShader(gl, gl.FRAGMENT_SHADER, fragment)
 
-createShader(gl, gl.VERTEX_SHADER, vsource)
-createShader(gl, gl.FRAGMENT_SHADER, fsource)
+  /* eslint-disable no-console */
+  console.log(vshader, fshader)
+})
