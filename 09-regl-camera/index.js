32066e89c6b2c40274574618315ef3fafaf86d95
Caged <justin@labratrevenge.com>
2018-05-21 20:41:16 -0700

    add orbital camera example using regl-camera

---
+++ 09-regl-camera/index.js
@@ -0,0 +1,60 @@
+const regl = require('regl')(document.body)
+const camera = require('regl-camera')(regl, {
+  center: [0, 0, 0],
+  theta: 1.5,
+  distance: 3
+})
+// You can't actually tweak this on many environments.  Drawing thick lines with
+// webgl is much more complex.  See https://github.com/jpweeks/regl-line-builder for a
+// convenient API
+const lineWidth = 1
+
+const lines = regl.elements({
+  primitive: 'lines',
+  // data represents the index of the `position` data
+  data: [[0, 1], [1, 2], [2, 3], [3, 0]]
+})
+
+const drawLines = regl({
+  frag: `
+    precision mediump float;
+    uniform vec4 color;
+    void main() {
+      gl_FragColor = color;
+    }`,
+
+  vert: `
+    precision mediump float;
+    attribute vec2 position;
+    uniform mat4 projection;
+    uniform mat4 view;
+
+    void main() {
+      gl_Position = projection * view * vec4(position, 0, 1);
+    }`,
+
+  attributes: {
+    position: [
+      [0.5, 0.5], // 0
+      [0.5, -0.5], // 1
+      [-0.5, -0.5], // 2
+      [-0.5, 0.5] // 3
+    ]
+  },
+
+  elements: lines,
+
+  uniforms: {
+    color: [1, 0, 0, 1]
+  },
+
+  lineWidth
+})
+
+regl.frame(() => {
+  camera(state => {
+    if (!state.dirty) return
+    regl.clear({color: [0, 0, 0, 1]})
+    drawLines({})
+  })
+})
