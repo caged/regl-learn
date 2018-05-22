127e2d2d3a0738a1a9e1af5d30a45e6ce306292c
Caged <justin@labratrevenge.com>
2018-05-22 08:47:06 -0700

    increase particle size in 03

--- 03-particles/index.js
+++ 03-particles/index.js
@@ -25,7 +25,7 @@ const drawPoints = regl({
     attribute vec2 position;
 
     void main() {
-      gl_PointSize = 2.0;
+      gl_PointSize = 4.0;
       gl_Position = vec4(position, 0, 1);
     }
   `,
