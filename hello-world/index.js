2c2081260ca46ad554a64418267998448ba177ea
Caged <justin@labratrevenge.com>
2018-05-21 19:06:20 -0700

    eslint hell-world

--- hello-world/index.js
+++ hello-world/index.js
@@ -5,19 +5,16 @@ function createShader(gl, type, source) {
   const shader = gl.createShader(type)
   gl.shaderSource(shader, source)
   gl.compileShader(shader)
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
   if (success) {
     return shader
   }
 
-  console.log(gl.getShaderInfoLog(shader))
   gl.deleteShader(shader)
 }
 
 const vsource = document.getElementById('vertex').text
 const fsource = document.getElementById('frag').text
 
-const vshader = createShader(gl, gl.VERTEX_SHADER, vsource)
-const fshader = createShader(gl, gl.FRAGMENT_SHADER, fsource)
-
-console.log(vshader);
+createShader(gl, gl.VERTEX_SHADER, vsource)
+createShader(gl, gl.FRAGMENT_SHADER, fsource)
