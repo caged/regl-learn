bde6ca002542afd3e665287e8613e77a3267d2a2
Caged <justin@labratrevenge.com>
2018-05-26 09:53:53 -0700

    first suitable version of from to animation

--- 11-animate-from-to/index.js
+++ 11-animate-from-to/index.js
@@ -2,7 +2,7 @@ const regl = require('regl')(document.body)
 const d3 = require('d3')
 
 // How many points to draw
-const numPoints = 500
+const numPoints = 5000
 
 // Size of vertex data in bytes (4.0 is GL_FLOAT size)
 const vertSize = 4 * 11
@@ -11,15 +11,13 @@ const vertSize = 4 * 11
 let pointCount = 0
 
 // point increment
-const pointIncrement = 1
+const pointIncrement = 2
 
 // random number generator for position
-const rng = d3.randomNormal(0.0001, 0.0001)
+const rng = d3.randomNormal(0.001, 0.1)
 
 // random number generator for velocity
-const rngv = d3.randomNormal(0.0001, 0.001)
-
-// let pointData = []
+const rngv = d3.randomNormal(0.1, 0.5)
 
 // Allocate a dynamic buffer that can store
 // our points
@@ -28,12 +26,6 @@ const points = regl.buffer({
   type: 'float',
   length: vertSize * numPoints
 })
-
-// Debug container
-// const debug = d3
-//   .select(document.body)
-//   .append('div')
-//   .attr('style', `padding:10px;background-color:#fff;position:absolute;right:0;top:0;color:green;font-family:courier`)
 
 const drawPoints = regl({
   depth: {enable: false},
@@ -58,20 +50,21 @@ const drawPoints = regl({
     attribute float birth;
     attribute vec4 color;
     uniform float tick;
+    uniform float time;
     varying vec4 fill;
 
 
     void main() {
       vec2 d = endpos - position;
       // vec2 newpos = (position + d + velocity) * (tick / birth);
-      vec2 newpos = position + velocity * tick;
-      // if(newpos.x >= endpos.x && newpos.y >= endpos.y) {
-      //   gl_PointSize = 0.0;
-      // } else {
-      //   gl_PointSize = 4.0;
-      // }
 
-      gl_PointSize = 8.0;
+      // Works with consitent velocity towards endpos
+      // vec2 newpos = (position + d) * (time - birth);
+
+      // Works with consitent velocity towards endpos
+      vec2 newpos = position + (d * ((time - birth) / 3.0));
+
+      gl_PointSize = 10.0;
       gl_Position = vec4(newpos, 0, 1);
       fill = color;
     }
@@ -110,7 +103,8 @@ const drawPoints = regl({
   },
 
   uniforms: {
-    tick: ({tick}) => tick
+    tick: regl.context('tick'),
+    time: regl.context('time')
   },
 
   // specify the number of points to draw
@@ -128,14 +122,14 @@ function makePoint(birth) {
       .map(() => {
         const {r, g, b} = d3.hsl(Math.random() * 60, 1, Math.max(0.2, Math.random() * 1)).rgb()
         return [
-          0.5, // x
-          0.5, // y
+          rng(), // x
+          rng(), // y
 
-          0.0, // endx
+          1.0, // endx
           0.0, // endy
 
-          0.001, // x velocity
-          0.001, // y velocity
+          rngv(), // x velocity
+          rngv(), // y velocity
 
           birth,
 
@@ -151,74 +145,12 @@ function makePoint(birth) {
   }
 }
 
-// Maybe fully replace buffer with new set of points that haven't
-// hit the endpos?
-function cullPoints(time, tick) {
-  // vec2 d = endpos - position;
-  // vec2 newpos = position + d * (tick - birth);
-  // points({
-  //   data:
-  // })
-  let numPoints = pointData.length
-
-  const tickSize = tick / 200
-  while (numPoints--) {
-    const point = pointData[numPoints]
-    // Start x
-    const [sx, sy] = point.slice(0, 2)
-    // End x, y
-    const [ex, ey] = point.slice(2, 4)
-    // Distance x, y
-    const dx = ex - sx
-    const dy = ey - sy
-
-    // Birth time
-    const birth = point[6]
-
-    const px = sx + dx * (tickSize - birth)
-    const py = sy + dy * (tickSize - birth)
-    // console.log(px, py, ex, ey)
-
-    // console.log(px, py, ex, ey)
-    if (px <= ex && py >= ey) {
-      pointData.splice(numPoints, 1)
-      // pointCount--
-    }
-  }
-  // for (const point of pointData) {
-  //   // Start x, y
-  //   const [sx, sy] = point.slice(0, 2)
-  //   // End x, y
-  //   const [ex, ey] = point.slice(2, 4)
-  //   // Distance x, y
-  //   const dx = ex - sx
-  //   const dy = ey - sy
-  //
-  //   // Birth time
-  //   const birth = point[6]
-  //
-  //   const px = sx + dx * (tick - birth)
-  //   const py = sy + dy * (tick - birth)
-  //   // console.log(px, py, ex, ey)
-  //   //
-  //   if (px <= ex && py >= ey) {
-  //     pointData.splice()
-  //   }
-  //
-  //   // console.log(epos)
-  //   // console.log(point)
-  // }
-}
-
-regl.frame(({time, tick}) => {
+regl.frame(({time}) => {
   regl.clear({
     color: [0, 0, 0, 1],
     depth: 1
   })
 
   drawPoints({points})
-  // cullPoints(time, tick)
-  if (tick % 20 == 0) {
-    makePoint(time)
-  }
+  makePoint(time)
 })
